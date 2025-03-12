2678cd7fddb5dc0533709202e5eda0ceedf250a0
sosa-04 <josue.sosaaleman@gmail.com>
2025-03-12 01:27:38 -0600

    Creacion de la estructura del proyecto

---
+++ src/main/java/hn/unah/ingenieriasf/swapping/SwappingApplication.java
@@ -0,0 +1,13 @@
+package hn.unah.ingenieriasf.swapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwappingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hn/unah/ingenieriasf/swapping/SwappingApplicationTests.java
@@ -0,0 +1,13 @@
+package hn.unah.ingenieriasf.swapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
